c4b5e9636cbff93bd6a8e74f00501a480f195ca9
somkakade <somanathkakade5@gmail.com>
2023-08-22 10:58:57 +0530

    added initel level of the code on the git hub

---
+++ app.js
@@ -0,0 +1,13 @@
+new Typed("#typed", {
+  strings: [
+    "Developer",
+    "Designer",
+    "Front End Enginner",
+    "Freelancer",
+    "YouTuber",
+  ],
+  typeSpeed: 100,
+  backSpeed: 20,
+  loop: true,
+  showCursor: true,
+});
